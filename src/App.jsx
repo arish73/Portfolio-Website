60198c1bdf1587122a9323f66ffb263279025999
Arish Izhar <ether@Arishs-MacBook-Pro.local>
2024-06-29 21:53:19 -0400

    added link buttons to socials, added download icon to resume button

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import './App.css';
 
 // Components
 import Navbar from './Components/navbar';
+import HeroSection from './Components/hero-section'
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <section>
+        <HeroSection />
+      </section>
 
     </div>
   );
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -7,21 +7,21 @@ const Navbar = () => {
       {/* flex container */}
       <div className="flex items-center justify-between">
         <div>
-        Arish 
-        </div>
+        ArishIzhar        </div>
       
       {/* menu items */}
       <div className="div flex space-x-5 ">
-        <a href="#" className="border-2 border-black px-2 py-1 rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50" >About Me</a>
-        <a href="#" className="border-2 border-black px-2 py-1 rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">Skills</a>
-        <a href="#" className="border-2 border-black px-2 py-1 rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">Experience</a>
-        <a href="#" className="border-2 border-black px-2 py-1 rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">Projects</a>
-        <a href="#" className="border-2 border-black px-2 py-1 rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">Contact Me</a>
+        <a href="#" className=" px-2 py-1 rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50" >About Me</a>
+        <a href="#" className=" px-2 py-1 rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">Skills</a>
+        <a href="#" className=" px-2 py-1 rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">Experience</a>
+        <a href="#" className=" px-2 py-1 rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">Projects</a>
+        <a href="#" className=" px-2 py-1 rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">Contact Me</a>
       </div>
 
       {/* Download resume button */}
       <a href="#" className="text-white bg-black rounded px-3 py-1 
-      hover:bg-white hover:text-black focus:outline-none focus:ring-7 focus:ring-blue-600 focus:ring-opacity-50 hover:ring-2 hover:ring-black">Resume</a>
+      hover:bg-white hover:text-black focus:outline-none focus:ring-7 focus:ring-blue-600 focus:ring-opacity-50 hover:ring-2 hover:ring-black">
+        <i className="fa-solid fa-download px-1"></i> Resume</a>
     </div>
 
 
