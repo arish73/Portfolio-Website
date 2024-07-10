2d0ba35a16cdfda26c1e13699910409c4118ebf2
Arish Izhar <ether@Arishs-MacBook-Pro.local>
2024-07-10 02:32:02 -0400

    modified file paths

--- src/Components/hero-section.jsx
+++ src/Components/hero-section.jsx
@@ -1,101 +1,3 @@
-// import React from 'react';
-
-
-
-// const HeroSection = () => {
-//   return (
-//     <div className="flex flex-row container mx-auto items-center pt-10">
-//         <div className="flex flex-col">
-//             <div className="whitespace-nowrap space-y-3 mt-32 justify-start">
-//                 <h1 class="text-5xl font-normal">Hello I’m <span class="font-bold">Arish Izhar</span></h1>
-//                 <h1 class="text-5xl font-extrabold"><span class="underline">Full Stack</span> <span className="font-mono outline-text">Developer</span></h1>
-//                 <h1 class="text-5xl font-normal">Based In <span class="font-bold">Canada.</span></h1>
-
-//             </div>
-            
-
-//             <p className="text-lg mt-6 mx-auto font-mono ">Creatively inclined & dedicated 
-//                 with a solid foundation in modern web technologies, 
-//                 focused on crafting seamless, user-friendly & high-performance applications 
-//                 </p>
-
-
-//             <div className="socials flex flex-row space-x-10 mt-36 justify-start">
-//                 <a href="https://github.com/arish73" target="_blank" rel="noopener noreferrer" className="w-16 h-16 flex items-center justify-center text-5xl border-2 p-2 border-black rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 transition duration-300">
-//                     <i className="devicon-github-original"></i>
-//                 </a>
-//                 <a href="https://www.linkedin.com/in/yourusername/" target="_blank" rel="noopener noreferrer" className="w-16 h-16 flex items-center justify-center text-5xl border-2 p-2 border-black rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 transition duration-300">
-//                     <i className="fa-brands fa-linkedin"></i>
-//                 </a>
-//                 <a href="mailto:izhar.arish@gmail.com" className="w-16 h-16 flex items-center justify-center text-5xl border-2 p-2 border-black rounded-lg hover:bg-black hover:text-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 transition duration-300">
-//                     <i className="fa-solid fa-envelope"></i>
-//                 </a>
-
-//             </div>
-//         </div>
-
-//         <div className="image flex-shrink-0">
-//             <img src="src/assets/frame-20-2.svg" alt="image" />
-//         </div>
-
-//     </div>
-//   );
-// };
-
-// export default HeroSection;
-
-
-// import React from 'react';
-// import { TypewriterEffectSmooth } from './ui/typewriter-effect';
-
-// const HeroSection = () => {
-//   const typewriterWords = [
-//     { text: "Full Stack", className: "text-5xl underline text-black" },
-//     { text: "Developer", className: "text-5xl font-extrabold font-mono outline-text" },
-    
-//   ];
-
-//   return (
-//     <div className="flex flex-row container mx-auto items-center pt-10">
-//       <div className="flex flex-col flex-grow"> {/* Ensure text color */}
-//         <div className="whitespace-nowrap space-y-3 mt-32">
-//           <h1 className="text-5xl font-normal text-black">Hello I’m <span className="font-bold">Arish Izhar</span></h1>
-//           <h1 className="text-5xl font-extrabold text-black">
-//             <TypewriterEffectSmooth words={typewriterWords} />
-//           </h1>
-//           <h1 className="text-5xl font-extrabold font-mono outline-text">
-//           Developer
-//           </h1>
-//           <h1 className="text-5xl font-normal text-black">Based In <span className="font-bold">Canada.</span></h1>
-//         </div>
-
-//         <p className="text-lg mt-6 font-mono text-black">
-//           Creatively inclined & dedicated with a solid foundation in modern web technologies, 
-//           focused on crafting seamless, user-friendly & high-performance applications
-//         </p>
-
-//         <div className="socials flex flex-row space-x-10 mt-20">
-//           <a href="https://github.com/arish73" target="_blank" rel="noopener noreferrer" className="w-16 h-16 flex items-center justify-center text-5xl border-2 p-2 border-black rounded-lg hover:bg-black hover:text-white transition duration-300">
-//             <i className="devicon-github-original"></i>
-//           </a>
-//           <a href="https://www.linkedin.com/in/yourusername/" target="_blank" rel="noopener noreferrer" className="w-16 h-16 flex items-center justify-center text-5xl border-2 p-2 border-black rounded-lg hover:bg-black hover:text-white transition duration-300">
-//             <i className="fa-brands fa-linkedin"></i>
-//           </a>
-//           <a href="mailto:izhar.arish@gmail.com" className="w-16 h-16 flex items-center justify-center text-5xl border-2 p-2 border-black rounded-lg hover:bg-black hover:text-white transition duration-300">
-//             <i className="fa-solid fa-envelope"></i>
-//           </a>
-//         </div>
-//       </div>
-
-//       <div className="image flex-shrink-0 relative">
-//         <img src="src/assets/f200.svg" alt="image" className="block" />
-//         <div className="line absolute bottom-0 md:left-[-20%] sm:left-[-20%] sm: md:w-[140%] lg:left-[-60%] lg:w-[170%] h-1 bg-black"></div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HeroSection;
 
 
 
@@ -139,7 +41,7 @@ const HeroSection = () => {
         </div>
 
         <div className="image flex-shrink-0 relative mb-8 md:mb-0 ">
-          <img src="src/assets/f200.svg" alt="image" className="block mx-auto" />
+          <img src="public/f200.svg" alt="image" className="block mx-auto" />
           <div className="line absolute bottom-0 left-0 w-full h-1 bg-black md:w-[140%] md:left-[-20%] lg:w-[160%] lg:left-[-50%]"></div>
         </div>
       </div>
