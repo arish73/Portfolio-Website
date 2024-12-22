eac23b85b64d19055e4eda3c600d3db4e035b993
Arish Izhar <ether@Arishs-MacBook-Pro.local>
2024-12-21 22:06:01 -0500

    resume butoon in mobile navbar

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = ({ onLinkClick, refs }) => {
         <div className="flex items-center justify-between">
           <div className="mx-2 alex-brush-regular font-bold">ArishIzhar</div>
 
+          {/* Hamburger button */}
           <button
             className="block mx-2 text-gray-900 focus:outline-none lg:hidden"
             id="menuButton"
@@ -35,6 +36,7 @@ const Navbar = ({ onLinkClick, refs }) => {
             </svg>
           </button>
 
+          {/* Desktop menu */}
           <div className="space-x-5 hidden lg:flex">
             <a
               className="cursor-pointer px-2 py-1 rounded-lg hover:bg-black hover:text-white transition duration-150"
@@ -68,12 +70,14 @@ const Navbar = ({ onLinkClick, refs }) => {
             </a>
           </div>
 
+          {/* Download Resume for Desktop */}
           <div className="hidden lg:block">
             <ResumeButton />
           </div>
         </div>
       </nav>
 
+      {/* Mobile menu */}
       {isMenuOpen && (
         <div className="flex flex-col lg:hidden m-2">
           <ul className="space-y-4">
@@ -118,6 +122,18 @@ const Navbar = ({ onLinkClick, refs }) => {
               </a>
             </li>
           </ul>
+
+          {/* Download Resume for Mobile */}
+          <div className="mt-4">
+            <a
+              href="/resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-2 text-white  bg-black rounded px-2 py-2 hover:bg-white hover:text-black focus:outline-none focus:ring-7 focus:ring-blue-600 focus:ring-opacity-50 hover:ring-2 hover:ring-black transition delay-50"
+            >
+              <i className="fa-solid fa-download px-1"></i> Resume
+            </a>
+          </div>
         </div>
       )}
     </div>
